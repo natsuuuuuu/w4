3239655c31f6d449dcc0c265a451d3695a969414
natsuuuuuu <113569431+natsuuuuuu@users.noreply.github.com>
2022-10-07 12:48:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ function result() {
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = `${Math.round(300*0.0714286)} stone`;
-    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    const weight = `${Math.round(300*0.0714286)} kg`;
+    const temperature =  `摂氏　${Math.round((94-32) * 5 / 9)} `;
     newStory = newStory.replace('華氏94', temperature);
     newStory = newStory.replace('300', weight);
   }
